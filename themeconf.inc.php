4c7ae70b75c97718195e7b3b8be8e0523773caab
klaoun <36087587+klaoun@users.noreply.github.com>
2021-04-27 14:27:33 +0200

    1er transfert

---
+++ themeconf.inc.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Theme Name: Montheme
+Version: 0.01
+Description: Mon premier theme
+Theme URI: http://piwigo.org/
+Author: klaoun
+Author URI: http://piwigo.org/
+*/
+$themeconf = array(
+  'name' => 'montheme',
+  'parent' => 'default',
+  'icon_dir' => 'themes/montheme/img',
+  'img_dir'      => 'themes/montheme/img',
+  'mime_icon_dir' => 'themes/montheme/img/',
+  'local_head' => '<!--template/local_head.tpl-->',
+  'activable' => 'true',
+  'load_parent_local_head' => 'false',
+  'load_parent_css' => 'false'
+);
+$conf['level_separator'] = ' &raquo; ';
+$conf['paginate_pages_around'] = 7;
+?>

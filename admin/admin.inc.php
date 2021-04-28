0e72c42ac2af44988f820d9f9d8730ef6bf0973d
klaoun <36087587+klaoun@users.noreply.github.com>
2021-04-28 16:14:18 +0200

    update

--- admin/admin.inc.php
+++ admin/admin.inc.php
@@ -1,5 +1,4 @@
 <?php
-//echo $_SERVER['HTTP_USER_AGENT'];
 $template->set_filenames(array('theme_admin_content' => dirname(__FILE__) . '/admin.tpl'));
 $template->assign('options', unserialize($conf['Ibufetum']));
 $template->assign_var_from_handle('ADMIN_CONTENT', 'theme_admin_content');

1e240726a37fd38fb46f6ba125cde955ee435839
klaoun <36087587+klaoun@users.noreply.github.com>
2021-04-28 19:09:04 +0200

    Update admin.inc.php

--- admin/admin.inc.php
+++ admin/admin.inc.php
@@ -1,5 +1,27 @@
 <?php
+// mimimum pour configuration
 $template->set_filenames(array('theme_admin_content' => dirname(__FILE__) . '/admin.tpl'));
 $template->assign('options', unserialize($conf['Ibufetum']));
 $template->assign_var_from_handle('ADMIN_CONTENT', 'theme_admin_content');
+// tabsheet
+include_once(PHPWG_ROOT_PATH . 'admin/include/tabsheet.class.php');
+define('THEME_ID', basename(dirname(dirname(__FILE__))));
+define('ADMIN_PATH',   get_root_url() . 'admin.php?page=theme&theme=' . THEME_ID);
+define('TAB_SETTINGS', 'Accueil');
+define('TAB_ABOUT', 'about');
+$page['tab'] = isset($_GET['tab']) ? $_GET['tab'] : $page['tab'] = TAB_SETTINGS;
+if (!in_array($page['tab'], array(TAB_SETTINGS, TAB_ABOUT))) {$page['tab'] = TAB_SETTINGS;
+$tabsheet = new tabsheet();
+$tabsheet->set_id('bsibu');
+$tabsheet->add(TAB_SETTINGS, l10n('Settings'), ADMIN_PATH . '&tab=' . TAB_SETTINGS);
+$tabsheet->add(TAB_ABOUT, l10n('About'), ADMIN_PATH . '&tab=' . TAB_ABOUT);
+$tabsheet->select($page['tab']);
+$tabsheet->assign();
+global $template;
+$template->set_filenames(
+    array(
+        'theme_admin_content' => dirname(__FILE__) . '/template/' . $page['tab'] . '.tpl'
+    )
+);
+$template->assign('theme_config', $themeconfig);
 ?>

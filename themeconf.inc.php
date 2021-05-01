b9188240c4436881e3cd041b6fb0923a3069c3e5
klaoun <36087587+klaoun@users.noreply.github.com>
2021-05-01 21:08:50 +0200

    Update themeconf.inc.php

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -14,7 +14,6 @@ $themeconf = array(
   'icon_dir' => 'themes/Ibufetum/img',
   'img_dir'      => 'themes/Ibufetum/img',
   'mime_icon_dir' => 'themes/Ibufetum/img/',
-  'local_head' => '<!--template/local_head.tpl-->',
   'activable' => 'true',
   'load_parent_local_head' => 'false',
   'load_parent_css' => 'false'

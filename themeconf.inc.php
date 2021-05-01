7f1e125065e20b3dec985a293df2fbdcd8465e35
klaoun <36087587+klaoun@users.noreply.github.com>
2021-05-01 21:01:07 +0200

    Add files via upload

---
+++ themeconf.inc.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Theme Name: Ibufetum master
+Version: auto
+Description: 1er thème personnalisé de piwigo
+Theme URI:
+Author: klaoun
+Author URI:
+Has Settings: true
+*/
+$themeconf = array(
+  'name' => 'Ibufetummaster',
+  'parent' => 'default',
+  'icon_dir' => 'themes/Ibufetum/img',
+  'img_dir'      => 'themes/Ibufetum/img',
+  'mime_icon_dir' => 'themes/Ibufetum/img/',
+  'local_head' => '<!--template/local_head.tpl-->',
+  'activable' => 'true',
+  'load_parent_local_head' => 'false',
+  'load_parent_css' => 'false'
+);
+$conf['level_separator'] = ' &raquo; ';
+$conf['paginate_pages_around'] = 7;
+?>

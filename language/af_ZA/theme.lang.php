d7e5b5dababa5e27a6fdcd4b5fddcf5aefc7173f
klaoun <36087587+klaoun@users.noreply.github.com>
2021-05-06 20:22:56 +0200

    Update and rename index.php to theme.lang.php

--- language/af_ZA/theme.lang.php
+++ language/af_ZA/theme.lang.php
@@ -5,11 +5,3 @@
 // | For copyright and license information, please view the COPYING.txt    |
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
-
-// Recursive call
-$url = '../';
-header( 'Request-URI: '.$url );
-header( 'Content-Location: '.$url );
-header( 'Location: '.$url );
-exit();
-?>

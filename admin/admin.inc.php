c3baa1f42d2c15be7a6bda120736c63579721361
klaoun <36087587+klaoun@users.noreply.github.com>
2022-10-23 21:07:42 +0200

    Update admin.inc.php

--- admin/admin.inc.php
+++ admin/admin.inc.php
@@ -1,6 +1,5 @@
 <?php
 $template->set_filenames(array('theme_admin_content' => dirname(__FILE__) . '/admin.tpl'));
-//$template->assign('options', unserialize($conf['Ibufetum']));
 $template->assign_var_from_handle('ADMIN_CONTENT', 'theme_admin_content');
 
 include_once(PHPWG_ROOT_PATH . 'admin/include/tabsheet.class.php');
